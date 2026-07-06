0f1d3061b1f4f5eba3f890a8444feaf8d8da3c1e
Andrei Serbanescu <andreiserbanescu35@gmail.com>
2026-07-06 14:56:19 +0300

    - Add 2 unit testing projects

--- src/CompanyEmployees.Data/Services/PermissionService.cs
+++ src/CompanyEmployees.Data/Services/PermissionService.cs
@@ -4,11 +4,6 @@ namespace CompanyEmployees.Data.Services;
 
 public static class PermissionService
 {
-    /// <summary>
-    /// Permisiunile efective = reuniunea (bitwise OR) permisiunilor tuturor rolurilor.
-    /// Administrator acordă orice permisiune. Necesită Roles încărcate
-    /// (ex. .Include(e => e.Roles)).
-    /// </summary>
     public static bool HasPermission(Employee employee, Permission permission)
     {
         var effective = employee.Roles
