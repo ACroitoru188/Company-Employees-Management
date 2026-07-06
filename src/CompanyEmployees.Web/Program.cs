1db41d2ad5e900c5140aebf2f24b39367fd1c1be
luca <luca.rusu@student.unitbv.ro>
2026-07-06 13:43:19 +0300

    Wired the Data project to the WebApp project

--- src/CompanyEmployees.Web/Program.cs
+++ src/CompanyEmployees.Web/Program.cs
@@ -1,10 +1,15 @@
+using CompanyEmployees.Data;
 using CompanyEmployees.Web.Components;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
